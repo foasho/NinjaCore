d54a2e7db9cc5a501b84d4e3fa654050b895320a
foasho <foa.sho0111@outlook.jp>
2023-12-01 10:02:15 +0900

    fix: builderror

--- src/lib/utils/NinjaInit.ts
+++ src/lib/utils/NinjaInit.ts
@@ -3,43 +3,28 @@ import { IConfigParams, IScriptManagement } from "./NinjaProps";
 
 export const InitMobileConfipParams: IConfigParams = {
   physics: true,
-  logarithmicDepthBuffer: false,
-  shadowResolution: 256,
-  alpha: false,
-  antialias: false,
-  autoScale: true,
-  mapsize: 64,
-  layerGridNum: 8,
-  lodDistance: 25,
   dpr: 1,
+  multi: true,
+  isApi: true,
+  isDebug: false,
 };
 
 export const InitTabletConfipParams: IConfigParams = {
   physics: true,
-  shadowResolution: 512,
-  alpha: false,
-  logarithmicDepthBuffer: false,
-  antialias: true,
-  autoScale: true,
-  mapsize: 256,
-  layerGridNum: 32,
-  lodDistance: 50,
   dpr: [1, 1.5],
+  multi: true,
+  isApi: true,
+  isDebug: false,
 };
 
 const isBrowser = typeof window !== "undefined";
 const dpr = isBrowser ? window.devicePixelRatio : 1;
 export const InitDesktopConfipParams: IConfigParams = {
   physics: true,
-  shadowResolution: 1024,
-  alpha: false,
-  logarithmicDepthBuffer: true,
-  antialias: true,
-  autoScale: true,
-  mapsize: 1024,
-  layerGridNum: 64,
-  lodDistance: 100,
   dpr: dpr,
+  multi: true,
+  isApi: true,
+  isDebug: false,
 };
 
 export const InitScriptManagement: IScriptManagement = {
@@ -47,4 +32,4 @@ export const InitScriptManagement: IScriptManagement = {
   name: "nonname-script" + MathUtils.generateUUID().substring(0, 6),
   type: "script",
   script: "",
-}
+};

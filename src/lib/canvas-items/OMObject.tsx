ca4f1953a6f29c7fe12a3db5dcb4bceb3128b964
foasho <foa.sho0111@outlook.jp>
2024-04-07 13:33:01 +0900

    fix: gltf

--- src/lib/canvas-items/OMObject.tsx
+++ src/lib/canvas-items/OMObject.tsx
@@ -82,7 +82,7 @@ const OMObject = React.memo(_OMObject,
  */
 const _LandScape = ({ om }: { om: IObjectManagement }) => {
   const ref = React.useRef<Group>(null);
-  const { nodes } = useGLTF(om.args.url as string) as GLTF;
+  const { nodes } = useGLTF(om.args.url as string) as GLTF && { nodes: any };
 
   React.useEffect(() => {
     if (ref.current) {
